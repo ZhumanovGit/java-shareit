d088a134036cd58a8caa8da2b50f9047f500f10a
ZhumanovGit <125205327+ZhumanovGit@users.noreply.github.com>
2023-12-31 14:03:54 +0300

    refactor: поправить временные зоны

--- server/src/main/java/ru/practicum/shareit/comment/dto/CommentCreateDto.java
+++ server/src/main/java/ru/practicum/shareit/comment/dto/CommentCreateDto.java
@@ -5,14 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentCreateDto {
-    @NotBlank
     private String text;
 }
--- server/src/main/java/ru/practicum/shareit/user/dto/UserCreateDto.java
+++ server/src/main/java/ru/practicum/shareit/user/dto/UserCreateDto.java
@@ -4,17 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 @Data
 @Builder
 @AllArgsConstructor
 public class UserCreateDto {
-    @NotBlank
     private String name;
-    @Email
-    @NotNull
     private String email;
 }
